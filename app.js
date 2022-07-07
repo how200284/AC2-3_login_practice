758f6ebb059c938b0ac6b1d37dd855013d55be4e
how200284 <how200284@gmail.com>
2022-07-07 16:25:34 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+
 const userList = require('./user.json')
 
 const app = express()
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Works!')
+  res.render('index')
+})
+
+app.get('/homepage', (req, res) => {
+  res.render('homepage')
 })
 
 app.listen(PORT, () => {
